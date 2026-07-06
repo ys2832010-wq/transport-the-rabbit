a400279100e70e608c640369d52bcc478c3d30a7
ys2832010-wq <ys2832010@gmail.com>
2026-07-06 16:10:06 +0300

    Delete عدد ساعات الكورس من عدد الثواني.py

--- عدد ساعات الكورس من عدد الثواني.py
+++
@@ -1,2 +0,0 @@
-Seconds=int(input("Enter the number of Seconds\n"))
-print (f"The number of hours is {Seconds//3600}, The number of miunets is {Seconds%60}, and The number of Seconds is {Seconds%60}")
