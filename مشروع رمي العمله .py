3e7b00dc84452ca226ffee0a4da614cee58a9889
ys2832010-wq <ys2832010@gmail.com>
2026-07-06 16:15:50 +0300

    Delete مشروع رمي العمله .py

--- مشروع رمي العمله .py
+++
@@ -1,8 +0,0 @@
-import random
-random_number=random.randint(0,1)
-print("Welcom to the virtual coin toss game")
-input("Press enter to start ....")
-if random_number== 0 :
-    print ("Heads")
-else :
-    print ("Teils")        
