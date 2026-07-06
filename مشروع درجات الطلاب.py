06a3217847ef259d452a57bbdf456b8fd82d609e
ys2832010-wq <ys2832010@gmail.com>
2026-07-06 16:11:12 +0300

    Delete مشروع درجات الطلاب.py

--- مشروع درجات الطلاب.py
+++
@@ -1,9 +0,0 @@
-number=int(input("Please enter the grade\n"))
-if number >= 90 :
-    print("Your appreciation is Excellent")
-elif number >=70 :
-    print("Your appreciation is Good")
-elif number >=50 :   
-    print("Your appreciation is Acceptable")
-else :
-    print("Your appreciation is Failing")    
