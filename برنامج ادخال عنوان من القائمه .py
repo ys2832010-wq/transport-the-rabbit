21df2b48e85e77ddc8356175f602c50d9755efed
ys2832010-wq <ys2832010@gmail.com>
2026-07-06 16:05:05 +0300

    Delete برنامج ادخال عنوان من القائمه .py

--- برنامج ادخال عنوان من القائمه .py
+++
@@ -1,5 +0,0 @@
-area=input("Choose one of the three titles,(Cairo), or (Alex), or (Tanta)\n").lower()
-if area== "cairo" or area== "alex" or area== "tanta" :
-    print (f"{area} is on our list ")
-else :
-    print(f"Sorry,{area} is no't our list ")    
