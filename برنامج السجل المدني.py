543794346b6670e6e2806303021b7226c4f8046a
ys2832010-wq <ys2832010@gmail.com>
2026-07-06 16:05:43 +0300

    Delete برنامج السجل المدني.py

--- برنامج السجل المدني.py
+++
@@ -1,9 +0,0 @@
-egyption=input("are you egyption? yes or no\n").lower()
-if egyption=="yes" :
-    age=input("are you adove 18? type yes or no\n")
-    if age =="yes" :
-        print("you can get me ID")
-    else :
-        print("you have to wait until you turn 18")
-else :
-    print("Sorry I can't help you ")            
